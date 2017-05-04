a905f84c20f128d83709134eea3b7a5643e517f4
Adrian Hintze <rydion00@gmail.com>
2017-05-05 01:46:02 +0200

    Added a new logo for the wiki. Updated the about section. Got rid of unnecessary dependencies. Rolled my own winston.d.ts.

--- src/client/components/about-component/about.component.ts
+++ src/client/components/about-component/about.component.ts
@@ -26,16 +26,16 @@ export default class AboutComponent {
         .size('lg')
         .title('About Snapp!')
         .body(`
-            <b>Snapp<i>!</i></b> was initially developed in 2015 by <a href="https://github.com/Rydion" target="_blank"><b>Adrian Hintze</b></a>.
+            <b>Snapp<i>!</i></b> was initially developed in 2015 by <a href="https://github.com/Rydion/" target="_blank" class="linked-name"><b>Adrian Hintze</b></a>.
 			<br><br>
-            Original idea by <a href="https://github.com/bromagosa" target="_blank" class="linked-name">Bernat Romagosa</a>
-            (<a href="http://edutec.citilab.eu/" target="_blank" class="linked-institution">Edutec</a>-<a href="http://citilab.eu/en" target="_blank" class="linked-institution">Citilab</a>).
+            Original idea by <a href="https://github.com/bromagosa/" target="_blank" class="linked-name">Bernat Romagosa</a>
+            (<a href="http://edutec.citilab.eu/" target="_blank" class="linked-institution">Edutec</a>-<a href="http://www.citilab.eu/" target="_blank" class="linked-institution">Citilab</a>).
 			<br><br>
-			<b>Snapp<i>!</i></b> is free software and available at <a href="https://github.com/Rydion/Snapp" target="_blank" class="linked-institution">GitHub</a>.
+			<b>Snapp<i>!</i></b> is free software and available at <a href="https://github.com/Rydion/Snapp/" target="_blank" class="linked-institution">GitHub</a>.
 			<br><br>
-			If you have any suggestions or find any issues with the application open a new GitHub issue or contact the author at:<br><b>hintze.adrian AT gmail DOT com</b>.
+			If you have any suggestions or find any issues with the application open a new <a href="https://github.com/Rydion/Snapp/issues/" target="_blank" class="linked-institution">GitHub issue</a> or contact the author at:<br><b>hintze.adrian AT gmail DOT com</b>.
             <br><br>
-            Version 2.0.0 - 2017-05-01
+            v2.0.0
         `)
         .open();
     }
